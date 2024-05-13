8deb53e96ed0be7484356e8494f5c67176f93da1
arbaz8668 <108510637+arbaz8668@users.noreply.github.com>
2024-05-13 16:11:43 +0530

    Update main.tf
    launching new instance t3large

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "Neml" {
-  ami           = "ami-0c32cf3c4c03c1b6c" # us-west-2
-  instance_type = "t3.xlarge"
+  ami           = "ami-0bfc1c8d33e1a8059" # us-west-2
+  instance_type = "t3.large"
   tags = {
       Name = "TF-Instance"
   }
